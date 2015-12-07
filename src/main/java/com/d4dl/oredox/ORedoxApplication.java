18694aef7f0ccfae5c2de975bed68403feaf8836
Joshua DeFord <joshua.deford@civitaslearning.com>
2015-12-07 09:44:34 -0600

    Initial spring boot commit

---
+++ src/main/java/com/d4dl/oredox/ORedoxApplication.java
@@ -0,0 +1,12 @@
+package com.d4dl.oredox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ORedoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ORedoxApplication.class, args);
+    }
+}
